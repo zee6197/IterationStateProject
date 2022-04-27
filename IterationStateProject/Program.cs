863a258228e95d44b4a05482221c0f3934ac33c7
zee6197 <zeeman@berkeley.edu>
2022-04-27 09:11:46 -0700

    First commit

---
+++ IterationStateProject/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+           /* for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }*/
+            
+
+            var numbers = new List<int>();
+            int num = 0;
+
+            do
+            {
+                num++;
+                numbers.Add(num);
+
+            } while (num < 100);
+
+
+            while (num < 200)
+            {
+                num++;
+                numbers.Add(num);
+            }
+
+
+            // This is to show the user that the numbers will start increasing on the console
+            Console.WriteLine("Increase:");
+
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Decrease:");
+
+
+            for (int i = 199; i >= 0 && i <= numbers.Count; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+    }
+}
